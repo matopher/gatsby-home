1df3d38304e1f6166dac6e7d772ef284b4a07dd1
Matt Woods <mattwoods21@gmail.com>
2017-11-22 19:49:45 -0600

    added gatsby plugin sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,8 @@ module.exports = {
   siteMetadata: {
     title: `Matt Woods`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`
+  ],
 }

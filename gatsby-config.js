39effba84997d3c859f7f27c7d8f988d92b97d32
Matt Woods <mattwoods21@gmail.com>
2018-02-28 02:08:45 -0600

    added medium source

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,12 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-medium`,
+      options: {
+        username: `@matopher`
+      },
+    },
   ],
 }

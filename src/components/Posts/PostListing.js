2fe0b8825fbd6efe4c06102a69ceac8acefce14c
Matt Woods <mw@mattwoods.io>
2018-07-04 14:08:41 -0500

    styled component update to PostListing

--- src/components/Posts/PostListing.js
+++ src/components/Posts/PostListing.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import styled from 'styled-components';
+
+const PostListingWrapper = styled.article`
+  margin-bottom: 1.5rem;
+  h3 {
+    margin-bottom: 0.5em;
+  }
+  a {
+    margin-top: 0.25em;
+    display: inline-block;
+    text-decoration: none;
+  }
+  p {
+    margin-bottom: 0;
+  }
+`;
 
 const PostListing = ({ post }) => (
-  <article>
+  <PostListingWrapper>
     <h3>
       <Link to={post.fields.slug}>{post.frontmatter.title}</Link>
     </h3>
     <span>{post.frontmatter.date}</span>
     <p>{post.excerpt}</p>
-  </article>
+    <Link to={post.fields.slug}>Read More &rarr;</Link>
+  </PostListingWrapper>
 );
 
 export default PostListing;

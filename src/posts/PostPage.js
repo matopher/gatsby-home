0f45b41b650d61b93f701a3e32564bbf9c83303e
Matt Woods <mw@mattwoods.io>
2018-12-18 23:46:55 -0600

    post padding style updates

--- src/posts/PostPage.js
+++ src/posts/PostPage.js
@@ -30,6 +30,24 @@ const Content = styled.div`
   max-width: 700px;
   margin: 0 auto;
   padding: 2em 1em;
+  @media screen and (max-width: 600px) {
+    padding: 1em 2em;
+  }
+  .post-date {
+    margin-bottom: 1.5em;
+    display: inline-block;
+    font-size: .9rem;
+    color: #808080;
+    display: inline-flex;
+    align-items: center;
+    svg {
+        max-height: 14px !important;
+        fill: #808080;
+    }
+  }
+  iframe {
+    max-width: 100%;
+  }
 `;
 
 export default class PostPage extends Component {
@@ -39,6 +57,9 @@ export default class PostPage extends Component {
       <BlogPost>
         <Content>
           <h1>{data.markdownRemark.frontmatter.title}</h1>
+          <span className="post-date">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm1-8.41l2.54 2.53a1 1 0 0 1-1.42 1.42L11.3 12.7A1 1 0 0 1 11 12V8a1 1 0 0 1 2 0v3.59z"/></svg>
+          {data.markdownRemark.frontmatter.date}</span>
           <div
             className="post-inner-content"
             dangerouslySetInnerHTML={{
@@ -59,6 +80,7 @@ export const query = graphql`
       html
       frontmatter {
         title
+        date(formatString: "MMMM DD, YYYY")
       }
       fields {
         slug
